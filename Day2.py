33e6d202b29e776b49471cb162dc88dcc96e62e9
Sheega-ameenah <sheegaamina@gmail.com>
2023-03-16 21:13:39 +0100

    Added Day2

---
+++ Day2.py
@@ -0,0 +1,8 @@
+#Write a Python script that prompts the user for their name and prints a personalized greeting.
+name = str(input (print("Please enter your name \n")))
+
+print (F"Hello {name}, welcome")
+
+        
+    
+
