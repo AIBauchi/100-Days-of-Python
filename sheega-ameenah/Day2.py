44a8422037e8407a0140132ab74a82499aeda2e3
Sheega-ameenah <sheegaamina@gmail.com>
2023-03-17 11:59:43 +0100

    Corrected

--- sheega-ameenah/Day2.py
+++ sheega-ameenah/Day2.py
@@ -1,7 +1,7 @@
 #Write a Python script that prompts the user for their name and prints a personalized greeting.
-name = str(input (print("Please enter your name \n")))
+name = input ("Please enter your name \n")
 
-print (F"Hello {name}, welcome")
+print (f"Hello {name}, welcome")
 
         
     
