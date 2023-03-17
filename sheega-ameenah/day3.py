01e8f58b0da0280616a7f6b57184bacf393f5156
Sheega-ameenah <sheegaamina@gmail.com>
2023-03-17 21:12:12 +0100

    added day3

---
+++ sheega-ameenah/day3.py
@@ -0,0 +1,5 @@
+# Write a Python script that calculates the area of a rectangle with user input.
+L = int(input("Please enter the length of the rectangle \n"))
+B = int (input("Enter the breadth too \n"))
+Area = L * B
+print(f"The area of the rectangle is {Area}")
