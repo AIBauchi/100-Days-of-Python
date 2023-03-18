60f60efc64c6488beb58acee8c6f1e96b6c17d9c
Sheega-ameenah <sheegaamina@gmail.com>
2023-03-18 20:54:56 +0100

    Corrected day3 and day4

--- sheega-ameenah/day3.py
+++ sheega-ameenah/day3.py
@@ -1,5 +1,5 @@
 # Write a Python script that calculates the area of a rectangle with user input.
-L = int(input("Please enter the length of the rectangle \n"))
-B = int (input("Enter the breadth too \n"))
-Area = L * B
-print(f"The area of the rectangle is {Area}")
+length = int(input("Please enter the length of the rectangle \n"))
+breadth = int (input("Enter the breadth too \n"))
+area = length * breadth
+print(f"The area of the rectangle is {area}")
--- sheega-ameenah/day4.py
+++ sheega-ameenah/day4.py
@@ -1,5 +1,5 @@
 #Write a Python script that calculates the area of a circle with user input
-import math
-r = int (input("Please enter the radius of the circle: "))
-Area = math.pi * pow(r,2)
-print(f"The area of the circle is {Area}")
+from math import pow,pi
+radius = int (input("Please enter the radius of the circle: "))
+area = pi * pow(radius,2)
+print(f"The area of the circle is {area}")
