6ed7c118a83191f320f268a6756824e4dd6ca0b7
Sheega-ameenah <sheegaamina@gmail.com>
2023-03-23 20:57:22 +0100

    Thank you for this opportunity

---
+++ sheega-ameenah/day9.py
@@ -0,0 +1,4 @@
+# Write a Python script that counts the number of words in a string with user input
+string = str(input("Hello , please enter the word, phrase or sentence you want to count:\n"))
+length = len(string)
+print(f"The total length is {length}")
