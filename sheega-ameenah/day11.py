b598f00cbb448e601490ecac03efe845570bdc6b
Sheega-ameenah <sheegaamina@gmail.com>
2023-03-25 21:10:24 +0100

    added day 10

---
+++ sheega-ameenah/day11.py
@@ -0,0 +1,5 @@
+# Write a Python script that removes all duplicates from a list with user input
+print('hello, enter the names of 5 people in your circlr \n')
+liist = {input("1 \n"),input("2 \n"),input("3 \n"),input("4 \n"),input("5 \n"),}
+
+print(liist)
